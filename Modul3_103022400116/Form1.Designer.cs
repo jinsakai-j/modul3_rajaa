7c96431b039759dcc747e8f65f04ba1e607d37e9
Rajaa Azharul <azharulrajaa@gmail.com>
2026-03-14 08:35:21 +0700

    Menambahkan implementasi konversi suhu

--- Modul3_103022400116/Form1.Designer.cs
+++ Modul3_103022400116/Form1.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Celcius", "Fahrenheit", "Kelvin", "Reamur" });
+            comboBox1.Location = new Point(77, 127);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(110, 33);
+            comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Celcius", "Fahrenheit", "Kelvin", "Reamur" });
+            comboBox2.Location = new Point(77, 249);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(110, 33);
+            comboBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(232, 129);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(343, 31);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(648, 127);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 4;
+            button1.Text = "Convert";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(77, 99);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 25);
+            label1.TabIndex = 5;
+            label1.Text = "Satuan Awal";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(77, 221);
+            label2.Name = "label2";
+            label2.Size = new Size(112, 25);
+            label2.TabIndex = 6;
+            label2.Text = "Satuan Akhir";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(232, 99);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 25);
+            label3.TabIndex = 7;
+            label3.Text = "Nilai Awal";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(232, 221);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 25);
+            label4.TabIndex = 8;
+            label4.Text = "Nilai Akhir";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(232, 249);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(343, 31);
+            textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox2;
     }
 }
--- Modul3_103022400116/Form1.cs
+++ Modul3_103022400116/Form1.cs
@@ -6,5 +6,58 @@ namespace Modul3_103022400116
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!double.TryParse(textBox1.Text, out double num1)) 
+            {
+                MessageBox.Show("Masukkan angka yang valid!");
+                return;
+
+            }else if(comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+            {
+                MessageBox.Show("Pilih satuan terlebih dahulu!");
+                return;
+            }
+
+            string dariSuhu = comboBox1.SelectedItem?.ToString();
+            string keSuhu = comboBox2.SelectedItem?.ToString();
+            double hasil = 0;
+
+            double celcius = 0;
+            switch (dariSuhu)
+            {
+                case "Celcius":
+                    celcius = num1;
+                    break;
+                case "Fahrenheit":
+                    celcius = (num1 - 32) * 5 / 9;
+                    break;
+                case "Kelvin":
+                    celcius = num1 - 273.15;
+                    break;
+                case "Reamur":
+                    celcius = num1 * 5 / 4;
+                    break;
+            }
+
+            switch (keSuhu)
+            {
+                case "Celcius":
+                    hasil = celcius;
+                    break;
+                case "Fahrenheit":
+                    hasil = (celcius * 9 / 5) + 32;
+                    break;
+                case "Kelvin":
+                    hasil = celcius + 273.15;
+                    break;
+                case "Reamur":
+                    hasil = celcius * 4 / 5;
+                    break;
+            }
+
+            textBox2.Text = hasil.ToString();
+        }
     }
 }
